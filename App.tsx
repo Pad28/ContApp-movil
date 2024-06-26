cb156a20d299ff9e8f916b7d1e9663501c252017
Pad28 <mipape2281003@gmail.com>
2024-06-26 01:13:37 -0600

    .

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { SettingsProvider } from './src/context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
     <AppState>
+      {/* <NavigationContainer>
+        
+      </NavigationContainer> */}
       <View style={{ marginTop: 50 }}>
         <Text>Hola mundo</Text>
       </View>
